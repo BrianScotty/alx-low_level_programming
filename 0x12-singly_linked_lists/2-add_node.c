18b2485fdc62d277be08409fde1caaf05e3d57d4
BrianScotty <scotty.brianleong@gmail.com>
2023-07-27 06:21:21 -0700

    task 0 and 2 corrections

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,14 +13,12 @@ list_t *add_node(list_t **head, const char *str)
 	unsigned int len = 0;
 
 	while (str[len])
-	{
 		len++;
+
 	node = malloc(sizeof(list_t));
-	}
 	if (!node)
-	{
 		return (NULL);
-	}
+
 	node->str = strdup(str);
 	node->len = len;
 	node->next = (*head);
